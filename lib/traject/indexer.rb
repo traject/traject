849989a31e23718a3365a0f4bf52a7420c9633ad
Jonathan Rochkind <jonathan@dnil.net>
2013-07-11 12:20:44 -0400

    move qualified_const_get to module; clean up indexer

--- lib/traject/indexer.rb
+++ lib/traject/indexer.rb
@@ -1,4 +1,5 @@
 require 'hashie'
+require 'traject/qualified_const_get'
 #
 #  == Readers and Writers
 #
@@ -34,6 +35,8 @@ require 'hashie'
 #  with a String name of class meeting the Writer contract. 
 #
 class Traject::Indexer
+  include Traject::QualifiedConstGet
+
   attr_writer :reader_class, :writer_class
 
   def initialize
@@ -185,28 +188,5 @@ class Traject::Indexer
     attr_accessor :clipboard, :output_hash
     attr_accessor :field_name, :source_record, :settings
   end
-
-  protected
-  # From http://redcorundum.blogspot.com/2006/05/kernelqualifiedconstget.html
-  def qualified_const_get(str)
-    path = str.to_s.split('::')
-    from_root = path[0].empty?
-    if from_root
-      from_root = []
-      path = path[1..-1]
-    else
-      start_ns = ((Class === self)||(Module === self)) ? self : self.class
-      from_root = start_ns.to_s.split('::')
-    end
-    until from_root.empty?
-      begin
-        return (from_root+path).inject(Object) { |ns,name| ns.const_get(name) }
-      rescue NameError
-        from_root.delete_at(-1)
-      end
-    end
-    path.inject(Object) { |ns,name| ns.const_get(name) }
-  end
-
 end
   
---
+++ lib/traject/qualified_const_get.rb
@@ -0,0 +1,30 @@
+# From http://redcorundum.blogspot.com/2006/05/kernelqualifiedconstget.html
+# Adapted into a module, rather than monkey patching it into Kernel
+#
+# Method to take a string constant name, including :: qualifications, and
+# look up the actual constant. Looks up relative to current file. 
+# REspects leading ::. Etc. 
+module Traject::QualifiedConstGet
+
+ 
+  def qualified_const_get(str)
+    path = str.to_s.split('::')
+    from_root = path[0].empty?
+    if from_root
+      from_root = []
+      path = path[1..-1]
+    else
+      start_ns = ((Class === self)||(Module === self)) ? self : self.class
+      from_root = start_ns.to_s.split('::')
+    end
+    until from_root.empty?
+      begin
+        return (from_root+path).inject(Object) { |ns,name| ns.const_get(name) }
+      rescue NameError
+        from_root.delete_at(-1)
+      end
+    end
+    path.inject(Object) { |ns,name| ns.const_get(name) }
+  end
+
+end
