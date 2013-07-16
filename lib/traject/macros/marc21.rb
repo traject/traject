07d3958063b6e2fae7cf43609da350ad959ec1fa
Jonathan Rochkind <jonathan@dnil.net>
2013-07-15 20:50:45 -0400

    Marc21.extract_marc trim_punctuation

--- lib/traject/macros/marc21.rb
+++ lib/traject/macros/marc21.rb
@@ -21,14 +21,40 @@ module Traject::Macros
     # to_field("id"),    extract_marc("001", :first => true)
     # to_field("geo"),   extract_marc("040a", :seperator => nil, :translation_map => "marc040")
     def extract_marc(spec, options = {})
-      only_first = options.delete(:first)
+      only_first        = options.delete(:first)
+      trim_punctuation  = options.delete(:trim_punctuation)
 
       lambda do |record, accumulator, context|
         accumulator.concat Traject::MarcExtractor.extract_by_spec(record, spec, options)
 
         # yeah, kind of esoteric, sorry. If ruby had an array.first! mutator, we'd use it.
         accumulator.slice!(1, accumulator.length) if only_first
+
+        # map
+
+        if trim_punctuation
+          accumulator.collect! {|s| Marc21.trim_punctuation(s)}
+        end
       end
+    end
+
+
+    # Trims punctuation mostly from end, and occasionally from beginning
+    # of string. Not nearly as complex logic as SolrMarc's version, just
+    # pretty simple.
+    #
+    # Removes
+    # * trailing: comma, slash, semicolon, colon (possibly followed by whitespace)
+    # * trailing period if it is preceded by at least three letters (possibly followed by whitespace)
+    # * single square bracket characters if they are the start and/or end
+    #   chars and there are no internal square brackets.
+    #
+    # Returns altered string, doesn't change original arg.
+    def self.trim_punctuation(str)
+      str = str.sub(/[ ,\/;:] *\Z/, '')
+      str = str.sub(/(\w\w\w)\. *\Z/, '\1')
+      str = str.sub(/\A\[?([^\[\]]+)\]?\Z/, '\1')
+      return str
     end
 
   end
--- test/indexer/macros_marc21_test.rb
+++ test/indexer/macros_marc21_test.rb
@@ -7,6 +7,8 @@ require 'traject/macros/marc21'
 # this is just a basic test to make sure our macro works passing through to there
 # and other options.
 describe "Traject::Macros::Marc21" do
+  Marc21 = Traject::Macros::Marc21 # shortcut
+
   before do
     @indexer = Traject::Indexer.new
     @indexer.extend Traject::Macros::Marc21
@@ -31,6 +33,32 @@ describe "Traject::Macros::Marc21" do
     output = @indexer.map_record(@record)
 
     assert_length 1, output["other_id"]
+  end
+
+  it "trims punctuation with :trim_punctuation => true" do
+    @indexer.instance_eval do 
+      to_field "title", extract_marc("245ab", :trim_punctuation => true)
+    end
+
+    output = @indexer.map_record(@record)
+
+    assert ! output["title"].first.end_with?("/"), "does not end with /"
+  end
+
+  it "Marc21::trim_punctuation class method" do
+    assert_equal "one two three", Marc21.trim_punctuation("one two three")
+
+    assert_equal "one two three", Marc21.trim_punctuation("one two three,")
+    assert_equal "one two three", Marc21.trim_punctuation("one two three/")
+    assert_equal "one two three", Marc21.trim_punctuation("one two three;")
+    assert_equal "one two three", Marc21.trim_punctuation("one two three:")
+    assert_equal "one two three .", Marc21.trim_punctuation("one two three .")
+    assert_equal "one two three", Marc21.trim_punctuation("one two three.")
+
+    assert_equal "one two [three]", Marc21.trim_punctuation("one two [three]")
+    assert_equal "one two three", Marc21.trim_punctuation("one two three]")
+    assert_equal "one two three", Marc21.trim_punctuation("[one two three")
+    assert_equal "one two three", Marc21.trim_punctuation("[one two three]")
   end
 
 end
