ee2152623eee940c350a07a7c47f6b0b801b4968
Jonathan Rochkind <jonathan@dnil.net>
2013-07-23 16:19:57 -0400

    whitespace

--- lib/traject/marc_reader.rb
+++ lib/traject/marc_reader.rb
@@ -1,17 +1,17 @@
 require 'marc'
 
 # A Reader class that can be used with Traject::Indexer.reader, to read
-# MARC records. 
+# MARC records.
 #
-# Includes Enumerable for convenience. 
+# Includes Enumerable for convenience.
 #
 # Reads in Marc records using ruby marc. Depends on config variables to
 # determine what serialization type to expect, and other parameters controlling
-# de-serialization. 
+# de-serialization.
 #
 # Settings:
 #   ["marc_source.type"]  serialization type. default 'binary'
-#                 * "binary". Actual marc. 
+#                 * "binary". Actual marc.
 #                 * "xml", MarcXML
 #                 * "json". (NOT YET IMPLEMENTED) The "marc-in-json" format, encoded as newline-seperated
 #                   json. A simplistic newline-seperated json, with no comments
@@ -22,10 +22,10 @@ require 'marc'
 #                              to use. Anything recognized by Marc::Reader :parser
 #                              argument. By default, asks Marc::Reader to take
 #                              it's best guess as to highest performance available
-#                              installed option. 
+#                              installed option.
 #
 #
-# Can NOT yet read Marc8, input is always assumed UTF8. 
+# Can NOT yet read Marc8, input is always assumed UTF8.
 class Traject::MarcReader
   include Enumerable
 
@@ -41,8 +41,8 @@ class Traject::MarcReader
   # Creates proper kind of ruby MARC reader, depending
   # on settings or guesses.
   def internal_reader
-    unless defined? @internal_reader      
-      @internal_reader = 
+    unless defined? @internal_reader
+      @internal_reader =
         case settings["marc_source.type"]
         when "xml"
           parser = settings["marc_source.xml_parser"] || @@best_xml_parser
