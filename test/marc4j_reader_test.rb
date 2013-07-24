f5e3fee0bf34c7f435343eafffc3cc8eb6774234
Jonathan Rochkind <jonathan@dnil.net>
2013-07-24 12:43:24 -0400

    Marc4JReader does XML

--- test/marc4j_reader_test.rb
+++ test/marc4j_reader_test.rb
@@ -20,7 +20,23 @@ describe "Marc4JReader" do
 
     first = array.first
 
-    assert_kind_of MARC::Record, first    
+    assert_kind_of MARC::Record, first
+    assert first['245']['a'].encoding.name, "UTF-8"
+    assert_equal "Fikr-i Ayāz /", first['245']['a']
+  end
+
+  it "reads XML" do
+    file = File.new(support_file_path "test_data.utf8.marc.xml")
+    settings = Traject::Indexer::Settings.new("marc_source.type" => "xml")
+    reader = Traject::Marc4JReader.new(file, settings)
+
+    array = reader.to_a
+
+    assert_equal 30, array.length
+
+    first = array.first
+
+    assert_kind_of MARC::Record, first
     assert first['245']['a'].encoding.name, "UTF-8"
     assert_equal "Fikr-i Ayāz /", first['245']['a']
   end
