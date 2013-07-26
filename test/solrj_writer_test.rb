08c69c1573e4f63adea7516aa2a25c83a6161674
Jonathan Rochkind <jonathan@dnil.net>
2013-07-25 20:47:17 -0400

    Phew, fix solrj_writer tests for batch/thread. Expected failure on raising exception under threaded at present

--- test/solrj_writer_test.rb
+++ test/solrj_writer_test.rb
@@ -2,6 +2,41 @@ require 'test_helper'
 
 require 'traject/solrj_writer'
 
+# create's a solrj_writer, maybe with MockSolrServer, maybe
+# with a real one. With settings in @settings, set or change
+# in before blocks
+#
+# writer left in @writer, with maybe mock solr server in @mock
+def create_solrj_writer
+  @writer = Traject::SolrJWriter.new(@settings)
+
+  if @settings["solrj_writer.server_class_name"] == "MockSolrServer"
+    # so we can test it later
+    @mock = @writer.solr_server
+  end
+end
+
+
+# Some tests we need to run multiple ties in multiple batch/thread scenarios, 
+# we DRY them up by creating a method to add the tests in different describe blocks
+def test_handles_errors
+  it "errors but does not raise on multiple ID's" do
+    @writer.put "id" => ["one", "two"]
+    @writer.close
+  end
+
+  it "errors and raises on connection error" do
+    @settings.merge!("solr.url" => "http://no.such.place")
+    create_solrj_writer
+
+    assert_raises org.apache.solr.client.solrj.SolrServerException do
+      @writer.put "id" => ["one"]
+      # in batch and/or thread scenarios, sometimes no exception raised until close
+      @writer.close
+    end
+  end
+end
+
 # WARNING. The SolrJWriter talks to a running Solr server.
 #
 # set ENV['solr_url'] to run tests against a real solr server
@@ -11,6 +46,23 @@ require 'traject/solrj_writer'
 #
 # This is pretty limited test right now.
 describe "Traject::SolrJWriter" do
+  $stderr.puts "WARNING: Testing SolrJWriter with mock instance" unless ENV["solr.url"]
+  before do
+    @settings = {
+      # Use XMLResponseParser just to test, and so it will work
+      # with a solr 1.4 test server
+      "solrj_writer.parser_class_name" => "XMLResponseParser",
+      "solrj_writer.commit_on_close" => "true",
+      "solrj_writer.batch_size" => nil
+    }
+
+    if ENV["solr_url"]
+      @settings["solr.url"] = ENV["solr_url"]
+    else
+      @settings["solr.url"] = "http://example.org/solr"
+      @settings["solrj_writer.server_class_name"] = "MockSolrServer"
+    end
+  end
 
   it "raises on missing url" do
     assert_raises(ArgumentError) { Traject::SolrJWriter.new }
@@ -26,30 +78,10 @@ describe "Traject::SolrJWriter" do
     assert_equal 100, Traject::SolrJWriter.new("solr.url" => "http://example.org/solr").settings["solrj_writer.batch_size"].to_i
   end
 
-  describe "with good setup" do
+  describe "with no threading or batching" do
     before do
-      @settings = {
-        # Use XMLResponseParser just to test, and so it will work
-        # with a solr 1.4 test server
-        "solrj_writer.parser_class_name" => "XMLResponseParser",
-        "solrj_writer.commit_on_close" => "true",
-        "solrj_writer.batch_size" => nil
-      }
-
-      if ENV["solr_url"]
-        @settings["solr.url"] = ENV["solr_url"]
-      else
-        $stderr.puts "WARNING: Testing SolrJWriter with mock instance"
-        @settings["solr.url"] = "http://example.org/solr"
-        @settings["solrj_writer.server_class_name"] = "MockSolrServer"
-      end
-
-      @writer = Traject::SolrJWriter.new(@settings)
-
-      if @settings["solrj_writer.server_class_name"] == "MockSolrServer"
-        # so we can test it later
-        @mock = @writer.solr_server
-      end
+      @settings.merge!("solrj_writer.batch_size" => nil, "solrj_writer.thread_pool" => nil)
+      create_solrj_writer
     end
 
     it "writes a simple document" do
@@ -71,47 +103,7 @@ describe "Traject::SolrJWriter" do
       end
     end
 
-    describe "with batching of solr docs" do
-      before do
-        @writer.settings["solrj_writer.batch_size"] = 5
-      end
-
-      it "sends all documents" do
-        docs = Array(1..17).collect do |i|
-          {"id" => ["item_#{i}"], "title" => ["To be #{i} again!"]}
-        end
-
-        docs.each do |doc|
-          @writer.put doc
-        end
-        @writer.close
-
-        if @mock
-          # 3 batches of 5, and the leftover 2 (16, 17)
-          assert_length 4, @mock.things_added
-
-          assert_length 5, @mock.things_added[0]
-          assert_length 5, @mock.things_added[1]
-          assert_length 5, @mock.things_added[2]
-          assert_length 2, @mock.things_added[3]
-        end
-      end
-    end
-
-    describe "with SolrJ Errors" do
-      it "errors but does not raise on multiple ID's" do
-        @writer.put "id" => ["one", "two"]
-        @writer.close
-      end
-
-      it "errors and raises on connection error" do
-        @writer = Traject::SolrJWriter.new(@settings.merge "solr.url" => "http://no.such.place")
-        assert_raises org.apache.solr.client.solrj.SolrServerException do
-          @writer.put "id" => ["one"]
-        end
-      end
-    end
-
+    test_handles_errors
 
 
     # I got to see what serialized marc binary does against a real solr server,
@@ -130,25 +122,88 @@ describe "Traject::SolrJWriter" do
         @writer.close
       end
     end
+  end
 
+  describe "with batching but no threading" do
+    before do
+      @settings.merge!("solrj_writer.batch_size" => 5, "solrj_writer.thread_pool" => nil)
+      create_solrj_writer
+    end
+
+    it "sends all documents" do
+      docs = Array(1..17).collect do |i|
+        {"id" => ["item_#{i}"], "title" => ["To be #{i} again!"]}
+      end
+
+      docs.each do |doc|
+        @writer.put doc
+      end
+      @writer.close
+
+      if @mock
+        # 3 batches of 5, and the leftover 2 (16, 17)
+        assert_length 4, @mock.things_added
+
+        assert_length 5, @mock.things_added[0]
+        assert_length 5, @mock.things_added[1]
+        assert_length 5, @mock.things_added[2]
+        assert_length 2, @mock.things_added[3]
+      end
+    end
+
+    test_handles_errors
+  end
+
+  describe "with batching and threading" do
+    before do
+      @settings.merge!("solrj_writer.batch_size" => 5, "solrj_writer.thread_pool" => 2)
+      create_solrj_writer
+    end
+
+    it "sends all documents" do
+      docs = Array(1..17).collect do |i|
+        {"id" => ["item_#{i}"], "title" => ["To be #{i} again!"]}
+      end
+
+      docs.each do |doc|
+        @writer.put doc
+      end
+      @writer.close
+
+      if @mock
+        # 3 batches of 5, and the leftover 2 (16, 17)
+        assert_length 4, @mock.things_added
+
+        # we can't be sure of the order under async,
+        # just three of 5 and one of 2
+        assert_length 3, @mock.things_added.find_all {|array| array.length == 5}
+        assert_length 1, @mock.things_added.find_all {|array| array.length == 2}
+      end
+    end
+
+    test_handles_errors
   end
 
 end
 
+require 'thread' # Mutex
 class MockSolrServer
   attr_accessor :things_added, :url, :committed, :parser, :shutted_down
 
   def initialize(url)
     @url =  url
     @things_added = []
+    @add_mutex = Mutex.new
   end
 
   def add(thing)
-    if @url == "http://no.such.place"
-      raise org.apache.solr.client.solrj.SolrServerException.new("bad uri", java.io.IOException.new)
-    end
+    @add_mutex.synchronize do # easy peasy threadsafety for our mock
+      if @url == "http://no.such.place"
+        raise org.apache.solr.client.solrj.SolrServerException.new("bad uri", java.io.IOException.new)
+      end
 
-    things_added << thing
+      things_added << thing
+    end
   end
 
   def commit
