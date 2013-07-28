07ee6f21ade17b71ab431e71d355bd6eed2d670d
Jonathan Rochkind <jonathan@dnil.net>
2013-07-28 17:47:13 -0400

    pull out #map_to_context!(context)

--- test/indexer/map_record_test.rb
+++ test/indexer/map_record_test.rb
@@ -107,16 +107,36 @@ describe "Traject::Indexer#map_record" do
         assert_kind_of Hash, context.clipboard
         assert_kind_of Hash, context.output_hash
 
+        assert_same @record, record
         assert_same record, context.source_record
         assert_same @indexer.settings, context.settings
-
-        assert_equal 10, context.position
       end
 
-      @indexer.map_record @record, :position => 10
+      @indexer.map_record @record
 
       assert called
     end
+  end
+
+  describe "map_to_context!" do
+    before do
+      @context = Traject::Indexer::Context.new(:source_record => @record, :settings => @indexer.settings, :position => 10 )
+    end
+    it "passes context to indexing routines"  do
+      called = false
+      @indexer.to_field("title") do |record, accumulator, context|
+        called = true
+        assert_kind_of Traject::Indexer::Context, context
+        assert_same @context, context
+      end
+
+      context = @indexer.map_to_context!(@context)
+
+      assert_same @context, context
+
+      assert called, "Called mapping routine"
+    end
+
   end
   
 end
