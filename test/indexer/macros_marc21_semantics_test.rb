3f290783da0c376ac9f6e7dda62b86680de9d985
Jonathan Rochkind <jonathan@dnil.net>
2013-07-29 17:08:08 -0400

    marc21semenatics macro, sortable_author

--- test/indexer/macros_marc21_semantics_test.rb
+++ test/indexer/macros_marc21_semantics_test.rb
@@ -27,4 +27,26 @@ describe "Traject::Macros::Marc21Semantics" do
 
     assert_equal %w{2710183 47971712},  output["oclcnum"]
   end
+
+  describe "sortable_author" do
+    # these probably should be taking only certain subfields, but we're copying
+    # from SolrMarc that didn't do so either and nobody noticed, so not bothering for now. 
+    before do
+      @indexer.instance_eval do
+        to_field "author_sort", sortable_author
+      end
+    end
+    it "collates author and title" do
+      output = @indexer.map_record(@record)
+
+      assert_equal ["Herman, Edward S.Manufacturing consent : the political economy of the mass media / Edward S. Herman and Noam Chomsky ; with a new introduction by the authors."], output["author_sort"]
+    end
+    it "respects non-filing" do
+      @record = MARC::Reader.new(support_file_path  "the_business_ren.marc").to_a.first
+
+      output = @indexer.map_record(@record)
+
+      assert_equal ["Business renaissance quarterly [electronic resource]."], output["author_sort"]
+    end
+  end
 end
