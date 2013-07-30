aeeef2b6c847f67e14939204ed94428bf5da07de
Jonathan Rochkind <jonathan@dnil.net>
2013-07-30 12:45:52 -0400

    marc21 semantics macro, publication_date

--- lib/traject/macros/marc21_semantics.rb
+++ lib/traject/macros/marc21_semantics.rb
@@ -187,5 +187,87 @@ module Traject::Macros
       end
     end
 
+    # An opinionated algorithm for getting a SINGLE publication date out of marc
+    #
+    # * Prefers using 008, but will resort to 260c
+    # * If 008 represents a date range, will take the midpoint of the range,
+    #     only if range is smaller than estimate_tolerance, default 15 years.
+    # * Ignores dates below min_year (default 500) or above max_year (this year plus 6 years),
+    #     because experience shows too many of these were in error.
+    #
+    # Yeah, this code ends up ridiculous. 
+    def marc_publication_date(options = {})
+      estimate_tolerance  = options[:estimate_tolerance] || 15
+      min_year            = options[:min_year] || 500
+      max_year            = options[:max_year] || (Time.new.year + 6)
+
+      lambda do |record, accumulator|
+        date = Marc21Semantics.publication_date(record, estimate_tolerance, min_year, max_year)
+        accumulator << date if date
+      end
+    end
+
+    # See #marc_publication_date. Yeah, this is a holy mess.
+    # Maybe it should actually be extracted to it's own class!
+    def self.publication_date(record, estimate_tolerance = 15, min_year = 500, max_year = (Time.new.year + 6))
+      field008 = MarcExtractor.extract_by_spec(record, "008").first
+      found_date = nil
+
+      if field008 && field008.length >= 11
+        date_type = field008.slice(6)
+        date1_str = field008.slice(7,4)
+        date2_str = field008.slice(11, 4) if field008.length > 15
+
+        # for date_type q=questionable, we have a range.
+        if (date_type == 'q')
+          # make unknown digits at the beginning or end of range,
+          date1 = date1_str.sub("u", "0").to_i
+          date2 = date2_str.sub("u", "9").to_i
+          # do we have a range we can use?
+          if (date2 > date1) && ((date2 - date1) <= estimate_tolerance)
+            found_date = (date2 + date1)/2
+          end
+        end
+        # didn't find a date that way, and anything OTHER than date_type
+        # n=unknown, q=questionable, try single date -- for some date types,
+        # there's a date range between date1 and date2, yeah, we often take
+        # the FIRST date then, the earliest. That's just what we're doing. 
+        if found_date.nil? && date_type != 'n' && date_type != 'q'
+          # in date_type 'r', second date is original publication date, use that I think?
+          date_str = (date_type == 'r' && date2_str.to_i != 0) ? date2_str : date1_str
+          # Deal with stupid 'u's, which end up meaning a range too,
+          # find midpoint and make sure our tolerance is okay.
+          ucount = 0
+          while (!date_str.nil?) && (i = date_str.index('u'))
+            ucount += 1
+            date_str[i] = "0"
+          end
+          date = date_str.to_i
+          if ucount > 0 && date != 0
+            delta = 10 ** ucount # 10^ucount, expontent
+            if delta <= estimate_tolerance
+              found_date = date + (delta/2)
+            end
+          elsif date != 0
+            found_date = date
+          end
+        end
+      end
+      # Okay, nothing from 008, try 260
+      if found_date.nil?
+        v260c = MarcExtractor.extract_by_spec(record, "260c", :seperator => nil).first
+        # just try to take the first four digits out of there, we're not going to try
+        # anything crazy.
+        if v260c =~ /(\d{4})/
+          found_date = $1.to_i
+        end
+      end
+
+      # is it within our acceptable range?
+      found_date = nil if found_date && (found_date < min_year || found_date > max_year)
+
+      return found_date
+    end
+
   end
 end
