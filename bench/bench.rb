d9eefedda79b908a7f147f0020ed157ccb8d454e
BillDueber <bill@dueber.com>
2013-08-28 11:26:17 -0400

    Normalize/DRY benchmarking code

--- bench/bench.rb
+++ bench/bench.rb
@@ -6,18 +6,18 @@ require 'benchmark'
 filename = 'bench.mrc'
 
 config_files = %w[
-  extract_marc_0_thread.rb
-  extract_marc2_0_thread.rb
-  extract_marc_3_thread.rb
-  extract_marc2_3_thread.rb
+  extract_marc_conf.rb
+  extract_marc2_conf.rb
 ]
 
 puts RUBY_DESCRIPTION
 Benchmark.bmbm do |x|
-  config_files.each do |cf|
-    x.report(cf) do
-      cmdline = Traject::CommandLine.new(["-c", cf, '--log.file', 'bench.log', filename])
-      cmdline.execute
+  [0, 3].each do |threads|
+    config_files.each do |cf|
+      x.report("#{cf} (#{threads})") do
+        cmdline = Traject::CommandLine.new(["-c", cf, '-s', 'log.file=bench.log', '-s', "processing_thread_pool=#{threads}", filename])
+        cmdline.execute
+      end
     end
   end
 end
