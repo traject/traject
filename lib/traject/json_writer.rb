3baeecefed5f53596057f30584f9324655af29a6
Jonathan Rochkind <jonathan@dnil.net>
2013-08-28 14:25:21 -0400

    trim whitespace

--- lib/traject/json_writer.rb
+++ lib/traject/json_writer.rb
@@ -4,18 +4,18 @@ require 'thread'
 # A writer for Traject::Indexer, that just writes out
 # all the output as Json. It's newline delimitted json, but
 # right now no checks to make sure there is no internal newlines
-# as whitespace in the json. TODO, add that. 
+# as whitespace in the json. TODO, add that.
 #
 # Should be thread-safe (ie, multiple worker threads can be calling #put
-# concurrently), by wrapping write to actual output file in a mutex synchronize. 
+# concurrently), by wrapping write to actual output file in a mutex synchronize.
 # This does not seem to effect performance much, as far as I could tell
-# benchmarking. 
+# benchmarking.
 #
 # You can force pretty-printing with setting 'json_writer.pretty_print' of boolean
-# true or string 'true'.  Useful mostly for human checking of output. 
+# true or string 'true'.  Useful mostly for human checking of output.
 #
 # Output will be sent to settings["output_file"] string path, or else
-# settings["output_stream"] (ruby IO object), or else stdout. 
+# settings["output_stream"] (ruby IO object), or else stdout.
 class Traject::JsonWriter
   attr_reader :settings
   attr_reader :write_mutex
@@ -31,7 +31,7 @@ class Traject::JsonWriter
   def put(context)
     hash = context.output_hash
 
-    serialized = 
+    serialized =
       if settings["json_writer.pretty_print"]
         JSON.pretty_generate(hash)
       else
@@ -44,7 +44,7 @@ class Traject::JsonWriter
 
   def output_file
     unless defined? @output_file
-      @output_file = 
+      @output_file =
         if settings["output_file"]
           File.open(settings["output_file"], 'w:UTF-8')
         elsif settings["output_stream"]
@@ -56,7 +56,7 @@ class Traject::JsonWriter
     return @output_file
   end
 
-  def close 
+  def close
     @output_file.close unless (@output_file.nil? || @output_file.tty?)
   end
 
