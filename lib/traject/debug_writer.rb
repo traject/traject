ff33f5cac2813b374af244eb1c16c89008aa5879
BillDueber <bill@dueber.com>
2013-08-30 10:42:56 -0400

    Make generic line writer; use it for JSON and a debug output

---
+++ lib/traject/debug_writer.rb
@@ -0,0 +1,17 @@
+require 'traject/line_writer'
+
+# A writer for Traject::Indexer that outputs each record as a series of
+# lines, prefixed by the id, one for each field and it's values.
+# Multiple values are separated by pipes
+
+class Traject::DebugWriter < Traject::LineWriter
+
+  def serialize(context)
+    idfield = settings["debug_writer.idfield"] || 'id'
+    format  = settings['debug_writer.format'] || '%-12s %-12s %s'
+    h = context.output_hash
+    lines = h.keys.sort.map {|k| format % [h[idfield].first, k, h[k].join(' | ')] }
+    lines.join("\n").strip
+  end    
+
+end
--- lib/traject/json_writer.rb
+++ lib/traject/json_writer.rb
@@ -1,63 +1,23 @@
 require 'json'
-require 'thread'
+require 'traject/line_writer'
 
 # A writer for Traject::Indexer, that just writes out
 # all the output as Json. It's newline delimitted json, but
 # right now no checks to make sure there is no internal newlines
 # as whitespace in the json. TODO, add that.
 #
-# Should be thread-safe (ie, multiple worker threads can be calling #put
-# concurrently), by wrapping write to actual output file in a mutex synchronize.
-# This does not seem to effect performance much, as far as I could tell
-# benchmarking.
-#
 # You can force pretty-printing with setting 'json_writer.pretty_print' of boolean
 # true or string 'true'.  Useful mostly for human checking of output.
-#
-# Output will be sent to settings["output_file"] string path, or else
-# settings["output_stream"] (ruby IO object), or else stdout.
-class Traject::JsonWriter
-  attr_reader :settings
-  attr_reader :write_mutex
 
-  def initialize(argSettings)
-    @settings     = argSettings
-    @write_mutex  = Mutex.new
+class Traject::JsonWriter < Traject::LineWriter
 
-    # trigger lazy loading now for thread-safety
-    output_file
-  end
-
-  def put(context)
+  def serialize(context)
     hash = context.output_hash
-
-    serialized =
-      if settings["json_writer.pretty_print"]
-        JSON.pretty_generate(hash)
-      else
-        JSON.generate(hash)
-      end
-      write_mutex.synchronize do
-        output_file.puts(serialized)
-      end
-  end
-
-  def output_file
-    unless defined? @output_file
-      @output_file =
-        if settings["output_file"]
-          File.open(settings["output_file"], 'w:UTF-8')
-        elsif settings["output_stream"]
-          settings["output_stream"]
-        else
-          $stdout
-        end
+    if settings["json_writer.pretty_print"]
+      JSON.pretty_generate(hash)
+    else
+      JSON.generate(hash)
     end
-    return @output_file
-  end
-
-  def close
-    @output_file.close unless (@output_file.nil? || @output_file.tty?)
-  end
+  end    
 
 end
