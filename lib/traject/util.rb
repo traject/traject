15ff4f446fe294ff0c4ffb5e9a8a14f05f2c8ae2
Bill Dueber <bill@dueber.com>
2015-01-16 20:58:18 -0500

    Take out solrj loading code for inclusion in traject_solrj_writer gem.

--- lib/traject/util.rb
+++ lib/traject/util.rb
@@ -27,70 +27,12 @@ module Traject
     end
 
 
-    # Requires solrj jar(s) from settings['solrj.jar_dir'] if given, otherwise
-    # uses jars bundled with traject gem in ./vendor
+
+    # Ruby stdlib queue lacks a 'drain' function, we write one.
     #
-    # Have to pass in a settings arg, so we can check it for specified jar dir.
-    #
-    # Tries not to do the dirglob and require if solrj has already been loaded.
-    # Will define global constants with classes HttpSolrServer and SolrInputDocument
-    # if not already defined.
-    #
-    # This is all a bit janky, maybe there's a better way to do this? We do want
-    # a 'require' method defined somewhere utility, so multiple classes can
-    # use it, including extra gems. This method may be used by extra gems, so should
-    # be considered part of the API -- after it's called, those top-level
-    # globals should be available, and solrj should be loaded.
-    def self.require_solrj_jars(settings)
-      jruby_ensure_init!
-
-      tries = 0
-      begin
-        tries += 1
-
-        org.apache.solr
-        org.apache.solr.client.solrj
-
-        # java_import which we'd normally use weirdly doesn't work
-        # from a class method. https://github.com/jruby/jruby/issues/975
-        Object.const_set("HttpSolrServer", org.apache.solr.client.solrj.impl.HttpSolrServer) unless defined? ::HttpSolrServer
-        Object.const_set("SolrInputDocument", org.apache.solr.common.SolrInputDocument) unless defined? ::SolrInputDocument
-      rescue NameError  => e
-        included_jar_dir = File.expand_path("../../vendor/solrj/lib", File.dirname(__FILE__))
-
-        jardir = settings["solrj.jar_dir"] || included_jar_dir
-        Dir.glob("#{jardir}/*.jar") do |x|
-          require x
-        end
-        if tries > 1
-          raise LoadError.new("Can not find SolrJ java classes")
-        else
-          retry
-        end
-      end
-    end
-
-    # just does a `require 'java'` but rescues the exception if we
-    # aren't jruby, and raises a better error message.
-    # Pass in a developer-presentable name of a feature to include in the error
-    # message if you want.
-    def self.jruby_ensure_init!(feature = nil)
-      begin
-        require 'java'
-      rescue LoadError => e
-        feature ||= "A traject feature is in use that"
-        msg = if feature
-          "#{feature} requires jruby, but you do not appear to be running under jruby. We recommend `chruby` for managing multiple ruby installs."
-        end
-        raise LoadError.new(msg)
-      end
-    end
-
-    # Ruby stdlib queue lacks a 'drain' function, we write one. 
-    # 
     # Removes everything currently in the ruby stdlib queue, and returns
     # it an array.  Should be concurrent-safe, but queue may still have
-    # some things in it after drain, if there are concurrent writers. 
+    # some things in it after drain, if there are concurrent writers.
     def self.drain_queue(queue)
       result = []
 
@@ -102,7 +44,7 @@ module Traject
       rescue ThreadError
         # Need do nothing, queue was concurrently popped, no biggie
       end
-      
+
       return result
     end
 
