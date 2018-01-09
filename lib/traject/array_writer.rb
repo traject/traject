eaa7c17446c7f3252a1da13ec1256fa95abf2039
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-01-09 16:33:07 -0500

    consistent name of Traject::ArrayWriter

--- lib/traject/array_writer.rb
+++ lib/traject/array_writer.rb
@@ -1,33 +1,31 @@
 module Traject
-  class Indexer
-    # Uses #put method from the traject writer API to just accumulate
-    # output_hash'es in an array. Useful for testing, or for simple programmatic
-    # use.
-    #
-    # Useful with process_with:
-    #
-    #     indexer.process_with(source_array, ArrayWriter.new).values
-    #       # => array of output_hash's
-    #
-    # Recommend against using it with huge number of records, as it will
-    # of course store them all in memory.
-    class ArrayWriter
-      attr_reader :values, :contexts
+  # Uses #put method from the traject writer API to just accumulate
+  # output_hash'es in an array. Useful for testing, or for simple programmatic
+  # use.
+  #
+  # Useful with process_with:
+  #
+  #     indexer.process_with(source_array, ArrayWriter.new).values
+  #       # => array of output_hash's
+  #
+  # Recommend against using it with huge number of records, as it will
+  # of course store them all in memory.
+  class ArrayWriter
+    attr_reader :values, :contexts
 
-      def initialize
-        @values = []
-        @contexts = []
-      end
+    def initialize
+      @values = []
+      @contexts = []
+    end
 
-      def put(context)
-        contexts << context
-        values << context.output_hash
-      end
+    def put(context)
+      contexts << context
+      values << context.output_hash
+    end
 
-      def clear!
-        @contexts.delete
-        @values.delete
-      end
+    def clear!
+      @contexts.delete
+      @values.delete
     end
   end
 end
