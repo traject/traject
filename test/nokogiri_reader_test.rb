375819a278185fdaa772b7d392de365492a18163
Jonathan Rochkind <jonathan@dnil.net>
2018-06-18 23:43:48 -0400

    NokogiriReader: nokogiri namespace shenanigans for jruby

--- test/nokogiri_reader_test.rb
+++ test/nokogiri_reader_test.rb
@@ -16,14 +16,17 @@ describe "Traject::NokogiriReader" do
 
 
     manually_extracted = Nokogiri::XML.parse(File.open(@xml_sample_path)).xpath(@each_record_xpath, @namespaces)
-    # crazy hack, oh Nokogiri namespaces
     manually_extracted.collect do |node|
-      node["xmlns"] = node.namespace.href if node.namespace
+      # nokogiri makes it so hard to reliably get an Element to serialize to XML with all
+      # it's inherited namespace declerations. :(  We're only doing this for testing purposes
+      # anyway.  This may not handle everything, but handles what we need in the test right now
+      if node.namespace
+        node["xmlns"] = node.namespace.href
+      end
     end
 
     assert_length manually_extracted.size, yielded_records
     assert yielded_records.all? {|r| r.kind_of? Nokogiri::XML::Document }
-
     assert_equal manually_extracted.collect(&:to_xml), yielded_records.collect(&:root).collect(&:to_xml)
   end
 
@@ -50,6 +53,26 @@ describe "Traject::NokogiriReader" do
 
       it "reads" do
         shared_tests
+      end
+    end
+
+    describe "outer namespaces" do
+      it "are preserved" do
+        @reader = Traject::NokogiriReader.new(File.open(support_file_path("namespace-test.xml")), {
+          "nokogiri_reader.default_namespaces" => { mytop: "http://example.org/top" },
+          "nokogiri_reader.each_record_xpath" => "//mytop:record"
+        })
+        yielded_records = []
+        @reader.each { |record|
+          yielded_records << record
+        }
+
+        assert yielded_records.length > 0
+
+        expected_namespaces = {"xmlns"=>"http://example.org/top", "xmlns:a"=>"http://example.org/a", "xmlns:b"=>"http://example.org/b"}
+        yielded_records.each do |rec|
+          assert_equal expected_namespaces, rec.namespaces
+        end
       end
     end
   end
